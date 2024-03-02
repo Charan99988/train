cd373a7421c5e58d304a57c6e33167f701a47dd8
Charan99988 <charanraj444889@gmail.com>
2024-03-02 22:48:33 +0530

    hi welcome to my reepo

---
+++ src/javaprogram/Class2.java
@@ -0,0 +1,21 @@
+package javaprogram;
+
+import java.util.Scanner;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=sc.nextInt();
+		//Using Algorithm
+		int rev=0;
+		while(num!=0)
+		{
+			rev=rev*10+num%10;
+			num=num/10;
+		}
+		System.out.println("Reverse Numbers is:"+rev);
+	}
+
+}
